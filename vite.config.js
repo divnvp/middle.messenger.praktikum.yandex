496456ebfd73494a6da63f501c8618e73149d0cf
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-02 10:28:33 +0300

    Добавила Prettier и файлы .*ignore

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import eslintPlugin from 'vite-plugin-eslint';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-	plugins: [handlebars(), eslintPlugin()],
-	build: {
-		rollupOptions: {
-			input: {
-				app: './static/index.html'
-			}
-		}
-	},
-	server: {
-		open: './static/index.html'
-	}
+  plugins: [handlebars(), eslintPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './static/index.html'
+      }
+    }
+  },
+  server: {
+    open: './static/index.html'
+  }
 });
