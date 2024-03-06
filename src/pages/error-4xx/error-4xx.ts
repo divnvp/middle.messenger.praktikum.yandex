f497e8aa344b6241009bdd9e6044a72e710fa636
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-06 12:41:21 +0300

    Добавила страницы 500 и 400

---
+++ src/pages/error-4xx/error-4xx.ts
@@ -0,0 +1,2 @@
+import './error-4xx.scss';
+export { default as Error4xx } from './error-4xx.hbs?raw';
--- static/index.js
+++ static/index.js
@@ -10,7 +10,9 @@ const pages = {
   registration: [Pages.RegistrationPage],
   profile: [Pages.Profile],
   'profile-data': [Pages.ProfileData],
-  'change-password': [Pages.ChangePassword]
+  'change-password': [Pages.ChangePassword],
+  'error-500': [Pages.Error500],
+  'error-4xx': [Pages.Error4xx]
 };
 
 Object.entries(Components).forEach(([name, component]) => {
@@ -23,16 +25,23 @@ function navigate(page) {
   document.body.innerHTML = handlebarsCompile(args);
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('auth'));
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('page').length) {
+    const currentPage = JSON.stringify(localStorage.getItem('page'));
+    const parsedPage = Object.keys(pages).find(v => `"${v}"` === currentPage);
+    history.pushState('', '', parsedPage);
+    navigate(parsedPage);
+  }
+});
 
 document.addEventListener('click', e => {
   const page = e.target.getAttribute('page');
   if (page) {
     navigate(page);
+    history.pushState('', '', e.target.getAttribute('page'));
+    localStorage.setItem('page', e.target.getAttribute('page'));
 
     e.preventDefault();
     e.stopImmediatePropagation();
   }
 });
-// console.log(5);
-// export const a = 5;
