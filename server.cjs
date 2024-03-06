c32e897f2eb3d10e8592dfa14c78cb9f31008c5f
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-06 22:21:53 +0300

    Обновила файлы

--- server.cjs
+++ server.cjs
@@ -6,4 +6,9 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send('Hello World!');
+});
+
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
