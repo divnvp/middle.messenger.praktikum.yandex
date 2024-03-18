e941e8b716f486e229cc01e15af9a60a4ae31e02
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-18 17:06:50 +0300

    Адаптировала страницу профиля под Block

---
+++ src/pages/profile/page/profile.ts
@@ -0,0 +1,56 @@
+import { InputField } from '@/components/input-field';
+import { Link } from '@/components/link';
+import { Menu } from '@/components/menu';
+import { ProfilePage } from '@/pages/profile';
+import { render } from '@/shared/utils/render';
+import { v4 as uuid } from 'uuid';
+
+const profilePage = new ProfilePage('div', {
+  attr: {
+    class: 'profile-page'
+  },
+  menu: new Menu('div', {
+    attr: {
+      class: 'col menu'
+    }
+  }),
+  fields: [
+    new InputField('div', {
+      name: 'mail',
+      title: 'Почта',
+      id: uuid(),
+      type: 'text'
+    }),
+    new InputField('div', {
+      name: 'login',
+      title: 'Логин',
+      id: uuid(),
+      type: 'text'
+    }),
+    new InputField('div', {
+      name: 'first_name',
+      title: 'Имя',
+      id: uuid(),
+      type: 'text'
+    }),
+    new InputField('div', {
+      name: 'second_name',
+      title: 'Фамилия',
+      id: uuid(),
+      type: 'text'
+    }),
+    new InputField('div', {
+      name: 'phone',
+      title: 'Телефон',
+      id: uuid(),
+      type: 'text'
+    })
+  ],
+  links: [
+    new Link('div', { url: '/', text: 'Изменить данные' }),
+    new Link('div', { url: '/', text: 'Изменить пароль' }),
+    new Link('div', { url: '/', text: 'Назад' })
+  ]
+});
+
+render('#app', profilePage);
--- src/pages/profile/profile.ts
+++
@@ -1,4 +0,0 @@
-import './profile.scss';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export { default as Profile } from './profile.hbs?raw';
