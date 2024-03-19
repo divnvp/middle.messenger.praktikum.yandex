4d39b422cfc543fae41fcc472faab5fc5350c95e
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-19 13:00:56 +0300

    Добавила в сборку все страницы

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,13 @@ export default defineConfig({
       input: {
         index: '/index.html',
         auth: '/pages/auth/auth.html',
-        registration: '/pages/registration/registration.html'
+        registration: '/pages/registration/registration.html',
+        chats: '/pages/chats/chat.html',
+        'error-4xx': '/pages/error-4xx/error-4xx.html',
+        'error-500': '/pages/error-500/error-500.html',
+        profile: '/pages/profile/profile.html',
+        'profile-data': '/pages/profile-data/profile-data.html',
+        'change-password': '/pages/change-password/change-password.html'
       }
     }
   },
