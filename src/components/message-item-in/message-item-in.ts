01bd1ae95f785ab3b8b0ed4523bf01f0a78ff07b
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-19 12:59:49 +0300

    Адаптировала компонент "Входящее сообщение" под Block

---
+++ src/components/message-item-in/message-item-in.ts
@@ -0,0 +1,10 @@
+const messageItemTemplate = `
+<div class='message-item'>
+  <div class='message-item_in'>
+    {{message}}
+    <div class='message-item_in__time'>{{time}}</div>
+  </div>
+</div>
+`;
+
+export default messageItemTemplate;
