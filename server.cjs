649f40d9265f4de4e5f63ee248000ea4bcd5ecc4
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-20 13:26:17 +0300

    Правки для тестов

--- server.cjs
+++ server.cjs
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const { fileURLToPath } = require("url");
 
 const app = express();
 const PORT = 3000;
+
+const __dirname = path.dirname(import.meta.url);
 
 app.use(express.static(path.join(__dirname, './dist')));
 
