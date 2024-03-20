526cfe2083550210d2917fb79f589927acf92e1b
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-20 09:23:12 +0300

    Обновила компонент "Кнопка с иконкой"

--- src/components/button-icon/button-icon.ts
+++ src/components/button-icon/button-icon.ts
@@ -1,7 +1,7 @@
 import './button-icon.scss';
 
 const buttonWithIconTemplate = `
-<button class='button-icon__button-color'>
+<button class='button-icon__button-color' type="{{type}}" onclick="{{onclick}}">
   <img class='button-icon__img' src='{{icon}}' alt='Button icon image' />
 </button>
 `;
