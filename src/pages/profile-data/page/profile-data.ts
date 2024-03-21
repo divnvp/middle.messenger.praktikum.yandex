7bcd6a3d6afc2da0ca1157e303be1895925b187b
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-21 18:03:08 +0300

    Поправила страницу изменения данных профиля под Block

--- src/pages/profile-data/page/profile-data.ts
+++ src/pages/profile-data/page/profile-data.ts
@@ -1,3 +1,4 @@
+import { onValidate, onValidateSubmit } from '@/shared/utils/validators/validate';
 import { Button } from '@/components/button';
 import { InputField } from '@/components/input-field';
 import { Link } from '@/components/link';
@@ -6,9 +7,12 @@ import { ProfileDataPage } from '@/pages/profile-data';
 import { render } from '@/shared/utils/render';
 import { v4 as uuid } from 'uuid';
 
-const profilePage = new ProfileDataPage('div', {
+const profilePage = new ProfileDataPage('form', {
   attr: {
     class: 'profile-page'
+  },
+  events: {
+    submit: onValidateSubmit
   },
   menu: new Menu('div', {
     attr: {
@@ -17,30 +21,45 @@ const profilePage = new ProfileDataPage('div', {
   }),
   fields: [
     new InputField('div', {
+      events: {
+        blur: { event: onValidate, querySelector: 'input' }
+      },
       name: 'email',
       title: 'Почта',
       id: uuid(),
       type: 'text'
     }),
     new InputField('div', {
+      events: {
+        blur: { event: onValidate, querySelector: 'input' }
+      },
       name: 'login',
       title: 'Логин',
       id: uuid(),
       type: 'text'
     }),
     new InputField('div', {
+      events: {
+        blur: { event: onValidate, querySelector: 'input' }
+      },
       name: 'first_name',
       title: 'Имя',
       id: uuid(),
       type: 'text'
     }),
     new InputField('div', {
+      events: {
+        blur: { event: onValidate, querySelector: 'input' }
+      },
       name: 'second_name',
       title: 'Фамилия',
       id: uuid(),
       type: 'text'
     }),
     new InputField('div', {
+      events: {
+        blur: { event: onValidate, querySelector: 'input' }
+      },
       name: 'phone',
       title: 'Телефон',
       id: uuid(),
