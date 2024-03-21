3eb7e927e316591aa47b440d1ebbb26a3d1227f0
Diana Pimenova <dpimenova@diasoft.ru>
2024-03-21 17:59:11 +0300

    Поправила страницу профиля под Block

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,23 +1,8 @@
 import { Block } from '@/shared/utils/block';
-import { onLoad } from '@/shared/utils/load';
 import profileTemplate from '@/pages/profile/page/template';
 
 export class ProfilePage extends Block {
-  override addEvents() {
-    super.addEvents();
-    document.addEventListener('DOMContentLoaded', this.onLoad);
-  }
-
-  override removeEvents() {
-    super.removeEvents();
-    document.removeEventListener('DOMContentLoaded', this.onLoad);
-  }
-
   override render() {
     return this.compile(profileTemplate);
-  }
-
-  private onLoad() {
-    onLoad('profile');
   }
 }
