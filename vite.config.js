45eac81c1d7e6ef3cdd3e8b65dd3f4fd5d275392
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-03 13:50:14 +0300

    Удалила страницы из сборки

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,7 @@ export default defineConfig({
     outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
-        index: '/index.html',
-        registration: '/pages/registration/registration.html',
-        chats: '/pages/chats/chat.html',
-        'error-4xx': '/pages/error-4xx/error-4xx.html',
-        'error-500': '/pages/error-500/error-500.html',
-        profile: '/pages/profile/profile.html',
-        'profile-data': '/pages/profile-data/profile-data.html',
-        'change-password': '/pages/change-password/change-password.html'
+        index: '/index.html'
       }
     }
   },
