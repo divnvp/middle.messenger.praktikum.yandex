c9e45616eda39f068acaffab93f69d2db6d388f1
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-04 16:12:33 +0300

    Преобразовала HOC

--- src/shared/utils/connect.ts
+++ src/shared/utils/connect.ts
@@ -1,13 +1,26 @@
 import store, { StoreEvents } from '@/shared/storage/store';
 import { Block } from '@/shared/utils/block';
+import { isEquals } from '@/shared/utils/custom-utils/is-equals';
 import { IState } from '@/shared/models/state.interface';
 
-export function connect(Component: typeof Block, mapStateToProps: (state: IState) => IState) {
-  return class extends Component {
-    constructor(props: NonNullable<unknown>) {
-      super('', { ...props, ...mapStateToProps(store.getState()) });
+export function connect(mapStateToProps: (state: IState) => IState) {
+  return function (Component: typeof Block) {
+    return class extends Component {
+      constructor(props: NonNullable<unknown>) {
+        let state = mapStateToProps(store.getState());
 
-      store.on(StoreEvents.Updated, () => this.setProps({ ...mapStateToProps(store.getState()) }));
-    }
+        super('', { ...props, ...state });
+
+        store.on(StoreEvents.Updated, () => {
+          const newState = mapStateToProps(store.getState());
+
+          if (!isEquals(state, newState)) {
+            this.setProps({ ...newState });
+          }
+
+          state = newState;
+        });
+      }
+    };
   };
 }
