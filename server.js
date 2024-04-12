a0eb92216cbd680e2bb598c39a14a8dd4d62d6e2
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-12 10:47:13 +0300

    Правки для деплоя

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-app.use(express.static('./dist'));
-
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 
   res.status(200);
 });
