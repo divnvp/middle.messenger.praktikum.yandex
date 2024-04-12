6a5da8094e588b41cdb5af58559224178e635eb2
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-12 09:19:33 +0300

    Правка для тестов

--- server.cjs
+++ server.cjs
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static('./dist'));
 
 app.get('*', (req, res) => {
   res.sendFile('index.html', {
-    root: path.join(__dirname, 'dist')
+    root: path.join(__dirname, '..', 'dist')
   });
   res.status(200);
 });
