97398d3675634e488ba959de8ba984f50863269a
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-12 09:39:29 +0300

    Правка для тестов

--- server.cjs
+++ server.cjs
@@ -1,16 +1,19 @@
-const express = require('express');
-const path = require('path');
-
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+// const PORT = 3000;
+//
+// app.use(express.static('./dist'));
+//
+// app.get('*', (req, res) => {
+//   res.sendFile('index.html', {
+//     root: path.join(__dirname, 'dist')
+//   });
+//   res.status(200);
+// });
+//
+// app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+const express = require("express");
 const app = express();
-const PORT = 3000;
-
-app.use(express.static('./dist'));
-
-app.get('*', (req, res) => {
-  res.sendFile('index.html', {
-    root: path.join(__dirname, 'dist')
-  });
-  res.status(200);
-});
-
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+app.use(express.static("./index.html"));
