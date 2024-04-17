cdee5ec5313e11d22927aede4afe44c00cea4699
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-17 16:23:21 +0300

    Сделала тесты для роутера

--- src/shared/router/router.spec.ts
+++ src/shared/router/router.spec.ts
@@ -1,1 +1,47 @@
-describe('', () => {});
+import Block from '../utils/block';
+import { expect } from 'chai';
+import Router from './router';
+import sinon from 'sinon';
+
+describe('Роутер', () => {
+  const mock: typeof Block = class {
+    getContent = sinon.fake(() => document.createElement('div'));
+  } as unknown as typeof Block;
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Проверяем создание корректного инстанса роута', () => {
+    const routerInstance = Router.use('/', mock);
+    expect(routerInstance).to.eq(Router);
+  });
+
+  it('Проверяем корректное создание страницы на старте роутера', () => {
+    Router.use('/', mock).start();
+    expect(window.history.length).to.eq(1);
+  });
+
+  it('Проверяем корректное возвращение назад', () => {
+    Router.use('/', mock).start();
+    Router.back();
+    expect(window.history.length).to.eq(1);
+  });
+
+  it('Проверяем корректное возвращение вперед', () => {
+    Router.use('/', mock).start();
+    Router.forward();
+    expect(window.history.length).to.eq(1);
+  });
+
+  it('Проверяем корректное изменение URL при переходе по страницам', () => {
+    Router.use('/profile', mock);
+    Router.go('/profile');
+    expect(window.location.pathname).to.eq('/profile');
+  });
+
+  it('Проверяем вывод корректного пути при взятии текущего роута', () => {
+    Router.use('/profile', mock);
+    expect(Router.currentRoute).to.eq('/profile');
+  });
+});
