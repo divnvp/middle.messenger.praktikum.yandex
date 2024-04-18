cf58d81660d40cc70a2dfd51a68bc6e9e839d83c
Diana Pimenova <dpimenova@diasoft.ru>
2024-04-18 11:49:16 +0300

    Добавила тесты для HTTPTransport

--- mocha-config.mjs
+++ mocha-config.mjs
@@ -1,9 +1,12 @@
 import { JSDOM } from 'jsdom';
+import Sinon from 'sinon';
 
 const jsDOM = new JSDOM('<div id="root"></div>', {
   url: 'http://localhost:3000'
 });
 
+global.XMLHttpRequest = Sinon.useFakeXMLHttpRequest();
+
 global.window = jsDOM.window;
-global.document = jsDOM.window.document;
 global.DocumentFragment = jsDOM.window.DocumentFragment;
+global.document = jsDOM.window.document;
--- src/shared/services/http.spec.ts
+++ src/shared/services/http.spec.ts
@@ -1,1 +1,64 @@
-describe('', () => {});
+import Sinon, { SinonFakeXMLHttpRequest } from 'sinon';
+import { expect } from 'chai';
+import { HTTPTransport } from './http';
+
+describe('HTTP Transport', () => {
+  const xhr = Sinon.useFakeXMLHttpRequest();
+  const http = new HTTPTransport('/chats');
+  let fakeRequests: SinonFakeXMLHttpRequest[] = [];
+
+  beforeEach(() => {
+    xhr.onCreate = (request: SinonFakeXMLHttpRequest) => {
+      fakeRequests.push(request);
+    };
+  });
+
+  afterEach(() => {
+    fakeRequests = [];
+  });
+
+  it('Content-Type в headers должен быть равен application/json', () => {
+    http.post('', { title: '' });
+    expect(fakeRequests[0].requestHeaders['Content-Type']).to.include('application/json');
+  });
+
+  it('Параметр responseType должен быть равен json', () => {
+    http.post('', { title: '' });
+    expect((fakeRequests[0] as unknown as { responseType: '' }).responseType).to.include('json');
+  });
+
+  it('Метод GET должен отправлять GET-запрос', () => {
+    http.get('/user');
+    expect(fakeRequests[0].method).to.eq('GET');
+  });
+
+  it('Метод GET должен отправлять GET-запрос c query-параметрами', () => {
+    http.get('/user', { a: '1', b: '2' });
+    expect(fakeRequests[0].url).to.include('?a=1&b=2');
+  });
+
+  it('Метод PUT должен отправлять PUT-запрос', () => {
+    http.put('/users', { users: ['1'], chatId: 0 });
+    expect(fakeRequests[0].method).to.eq('PUT');
+  });
+
+  it('Метод PUT должен формировать requestBody с заданными параметрами', () => {
+    http.put('/users', { users: ['1'], chatId: 0 });
+    expect(fakeRequests[0].requestBody).to.eq(JSON.stringify({ users: ['1'], chatId: 0 }));
+  });
+
+  it('Метод POST должен отправлять POST-запрос', () => {
+    http.post('', { title: '' });
+    expect(fakeRequests[0].method).to.eq('POST');
+  });
+
+  it('Метод POST должен формировать requestBody с заданными параметрами', () => {
+    http.post('', { title: 'a' });
+    expect(fakeRequests[0].requestBody).to.eq(JSON.stringify({ title: 'a' }));
+  });
+
+  it('Метод DELETE должен отправлять DELETE-запрос', () => {
+    http.delete('/users', { users: ['1'], chatId: 0 });
+    expect(fakeRequests[0].method).to.eq('DELETE');
+  });
+});
